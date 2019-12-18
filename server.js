0a9e07109901a57c288b70bbe73539940c1e755e
Bren-lap <brennenbates2859@gmail.com>
2019-12-17 20:43:02 -0700

    Fixing file structure for handlebars to work
    modifications in server.js and file structure to enable handlebars functionality

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
@@ -13,11 +14,12 @@ var db = require("./models");
 
 var PORT = 3000;
 
-
 // Initialize Express
 var app = express();
 
 // Configure middleware
+
+
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
@@ -27,11 +29,22 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+//set up handlebars engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
 mongoose.connect("mongodb://localhost/scienceNews", { useNewUrlParser: true });
 
 // Routes
 
 app.get("/", function(req,res) {
-    res.render(index);
+    res.render("index");
 })
+
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
